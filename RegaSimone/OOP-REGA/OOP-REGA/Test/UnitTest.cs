a629f71a1f15d5326e3cee9c0f99a552b03a2453
SimoRega <simone.rega2@studio.unibo.it>
2021-05-07 17:05:35 +0200

    Task Finished

--- RegaSimone/OOP-REGA/OOP-REGA/Test/UnitTest.cs
+++ RegaSimone/OOP-REGA/OOP-REGA/Test/UnitTest.cs
@@ -15,7 +15,7 @@ namespace OOP_REGA
         }
 
         [Test]
-        public void CorrectItemList()
+        public void CorrectFoodList()
         {
             var test = s.GetFoodItemList();
             Assert.True(test.Contains(FoodType.APPLE));
@@ -28,6 +28,52 @@ namespace OOP_REGA
             Assert.True(test.Contains(FoodType.POTATO));
             Assert.True(test.Contains(FoodType.TOMATO));
             Assert.True(test.Contains(FoodType.WHEAT));
+            
+        }
+
+        [Test]
+        public void CorrectSeedList()
+        {
+            var test = s.GetSeedItemList();
+            Assert.True(test.Contains(SeedType.APPLE_SEED));
+            Assert.True(test.Contains(SeedType.CARROT_SEED));
+            Assert.True(test.Contains(SeedType.CHERRY_SEED));
+            Assert.True(test.Contains(SeedType.ORANGE_SEED));
+            Assert.True(test.Contains(SeedType.POTATO_SEED));
+            Assert.True(test.Contains(SeedType.TOMATO_SEED));
+            Assert.True(test.Contains(SeedType.WHEAT_SEED));
+        }
+
+        [Test]
+        public void BuySeedTest()
+        {
+            Seed seed  = s.buy(SeedType.APPLE_SEED);
+
+            Assert.AreEqual(seed, new SeedImpl(SeedType.APPLE_SEED));
+        }
+
+        [Test]
+        public void SellFoodTest()
+        {
+            double wallet = 0;
+            for(int i =0; i < 10; i ++)
+            {
+                wallet += s.sell(FoodType.APPLE);
+            }
+            Assert.AreEqual(wallet,1000);
+        }
+
+        [Test]
+        public void SellAllFoodTest()
+        {
+            Dictionary<FoodType, int> foodToSell = new Dictionary<FoodType, int>();
+            foodToSell.Add(FoodType.APPLE, 10);
+            foodToSell.Add(FoodType.CARROT, 10);
+            foodToSell.Add(FoodType.POTATO, 10);
+
+            double wallet = s.sellAll(foodToSell);
+
+            Assert.AreEqual(wallet, 1350);
         }
     }
 }
